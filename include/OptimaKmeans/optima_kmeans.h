2b4f5c2990d8fab4a2faa3dd50eb761599be5653
Jingwen <jpan82@wisc.edu>
2026-03-29 15:32:36 -0400

    basedline: change optima_malloc_clusters variable name

--- include/OptimaKmeans/optima_kmeans.h
+++ include/OptimaKmeans/optima_kmeans.h
@@ -6,6 +6,6 @@ int optima_load_data_bin(const char* filename, double** data, int* n, int* d);
 int optima_load_data_csv(const char* filename, double** data, int* n, int* d);
 double* optima_kmeans(double *points, int num_points, int dim, int k, int max_iter, int *clusters);
 void optima_free_data(double* data, double* centroids, int* clusters);
-void optima_malloc_clusters(int** clusters, int k);
+void optima_malloc_clusters(int** clusters, int n);
 
 #endif // OPTIMA_KMEANS_H
